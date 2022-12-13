8b41e39406d1bfa778b52f880fd9ac518d72895f
sravankumar3846 <120361953+sravankumar3846@users.noreply.github.com>
2022-12-13 14:13:35 +0530

    Create sravan5.py

---
+++ sravan5.py
@@ -0,0 +1,23 @@
+def test(str1):
+    return str1[len(str1)-2] in str1[len(str1)-1] and str1[len(str1)-2] != str1[len(str1)-1]
+
+str11 = ["a","abb","sfs", "oo", "de", "sfde"]
+print("Original list:")
+print(str11)
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
+str11 = ["a","abb","sfs", "oo", "ee", "sfde"]
+print("\nOriginal list:")
+print(str11)
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
+str11 = ["a","abb","sad", "ooaa" "esdfe", "sfsdfde", "sfsd", "sfsdf", "qwrew"]
+print("\nOriginal list:")
+print(str11)
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11)) 
+str11 = ["a","abb","sad", "ooaa" "esdfe", "sfsdfde", "sfsd", "sfsdf", "qwsfsdfrew"]
+print("\nOriginal list:")
+print(str11)
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
