b222e4184f5b91e5991f3c7809f91880fe70c93f
sravankumar3846 <120361953+sravankumar3846@users.noreply.github.com>
2022-12-13 14:11:42 +0530

    Create sravan4.py

---
+++ sravan4.py
@@ -0,0 +1,18 @@
+def test(n):    
+    return [n + 2 * i for i in range(n)]   
+n = 2
+print("Number of piles:",n)
+print("Number of stones in each pile:")
+print(test(n)) 
+n = 10
+print("\nNumber of piles:",n)
+print("Number of stones in each pile:")
+print(test(n)) 
+n = 3
+print("\nNumber of piles:",n)
+print("Number of stones in each pile:")
+print(test(n)) 
+n = 17
+print("\nNumber of piles:",n)
+print("Number of stones in each pile:")
+print(test(n))
